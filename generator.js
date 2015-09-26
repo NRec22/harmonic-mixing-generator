4d078a6635101d78ade62634ac6da76c2d9459fb
NRec22 <nevenrecchia@gmail.com>
2015-09-26 18:42:04 -0400

    basic matcher

--- generator.js
+++ generator.js
@@ -5,16 +5,32 @@ function storeTrack(playlist, title, num, key){
     playlist.push({title: title, num: num, key: key});
 }
 
-//given a playlist, this method creates a new playlist with matching or compatible songs
+//given a song, this method creates a new playlist with matching or compatible songs
 function listCompatible(playlist){
+    var newList = [];
+    var firstTrack = playlist.shift()
 
+    newList.push(firstTrack);
+
+    for(var i = 0; i < playlist.length; i++){
+        if(firstTrack.num == playlist[i].num && firstTrack.key == playlist[i].key){
+            newList.push(playlist[i]);
+        }
+    }
+
+
+    return newList;
 }
 
 //testing below here
 storeTrack(initialPlaylist, "Riders Of Rohan", 7, "m");
 storeTrack(initialPlaylist, "Stargazer", 9, "m");
+storeTrack(initialPlaylist, "Gamemaster", 2, "m");
 storeTrack(initialPlaylist, "Khepera", 7, "m");
 storeTrack(initialPlaylist, "Slave (feat. Fisher)", 8, "m");
+console.log("Initial Playlist");
 console.log(initialPlaylist);
 
+console.log("Matching number and key with first song");
+console.log(listCompatible(initialPlaylist));
 
