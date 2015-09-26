fd4d05bb26c0fc3c816579039802b4a65e915c1f
NRec22 <nevenrecchia@gmail.com>
2015-09-26 17:10:31 -0400

    refined store method

--- generator.js
+++ generator.js
@@ -1,18 +1,20 @@
-//playlist objects have a number (1-12) and a key(m/d) to represent Traktor's camelot wheel notation.
+//playlist objects have a number (1-12) and a key(m/d) to represent Traktor's camelot wheel notation
+var initialPlaylist = [];
 
-var playlist = [];
-
-function storeTrack(title, num, key){
+function storeTrack(playlist, title, num, key){
     playlist.push({title: title, num: num, key: key});
 }
 
+//given a playlist, this method creates a new playlist with matching or compatible songs
+function listCompatible(playlist){
 
+}
 
 //testing below here
-
-storeTrack("Riders Of Rohan", 7, "m");
-storeTrack("Khepera", 7, "m");
-storeTrack("Slave (feat. Fisher)", 8, "m");
-console.log(playlist);
+storeTrack(initialPlaylist, "Riders Of Rohan", 7, "m");
+storeTrack(initialPlaylist, "Stargazer", 9, "m");
+storeTrack(initialPlaylist, "Khepera", 7, "m");
+storeTrack(initialPlaylist, "Slave (feat. Fisher)", 8, "m");
+console.log(initialPlaylist);
 
 
