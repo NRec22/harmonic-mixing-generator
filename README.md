1754d90afe2e2890a55e3a12d5e5607fd259aec8
NRec22 <nevenrecchia@gmail.com>
2015-10-31 22:09:47 -0400

    reworked listCompatible

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Harmonic Playlist Generator
 
 Takes a list of songs and it's key on the camelot wheel and generates a playlist with each sequential track compatible by key with the previous track.
+
+* Generates playlist starting from the top towards the bottom. Priority is from the top.
+
+## To Do
+
+* Create a web application.
+* Add support for Mixed In Key's notation. (1A-12B)
+* Extended features (TBA)
--- generator.js
+++ generator.js
@@ -112,19 +112,15 @@ function checkCompatible(num1, key1, num2, key2){
 }
 
 
-//Given a playlist, this method creates a new playlist with matching or compatible songs based on the first song.
-function listCompatible(playlist){
+//Given a playlist, this method creates a new playlist with matching or compatible songs based on a given key.
+function listCompatible(playlist, num, key){
     var newList = [];
-    var firstTrack = playlist[0];
-
-    newList.push(firstTrack);
 
     for(var i = 0; i < playlist.length; i++){
-        if(checkCompatible(firstTrack.num, firstTrack.key, playlist[i].num, playlist[i].key)){
+        if(checkCompatible(num, key, playlist[i].num, playlist[i].key)){
             newList.push(playlist[i]);
         }
     }
-    newList.shift();
     return newList;
 }
 
@@ -157,92 +153,92 @@ function harmonicSortHelper(sortlist, unsortlist){
 
 
 //testing below here
-var initialPlaylist = [];
-storeTrack(initialPlaylist, "Riders Of Rohan", 7, "m");
-storeTrack(initialPlaylist, "Stargazer", 9, "m");
-storeTrack(initialPlaylist, "Gamemaster", 2, "m");
-storeTrack(initialPlaylist, "FAKE", 7, "d");
-storeTrack(initialPlaylist, "FAKE2", 6, "d" );
-storeTrack(initialPlaylist, "FAKE3", 10, "d");
-storeTrack(initialPlaylist, "FAKE4", 6, "m");
-storeTrack(initialPlaylist, "Khepera", 7, "m");
-storeTrack(initialPlaylist, "Slave (feat. Fisher)", 8, "m");
-console.log("Initial Playlist");
-//console.log(initialPlaylist);
-console.log("Compatible with first song");
-console.log(listCompatible(initialPlaylist));
-
-var initialPlaylist2 = [];
-storeTrack(initialPlaylist2, "here we go", 12, "m");
-storeTrack(initialPlaylist2, "jump on it", 1, "m");
-storeTrack(initialPlaylist2, "music and wine", 7, "m");
-storeTrack(initialPlaylist2, "turn up the bass", 11, "d");
-console.log("Initial Playlist 2");
-//console.log(initialPlaylist2);
-console.log("Compatible with first song");
-console.log(listCompatible(initialPlaylist2));
-
-var initialPlaylist3 = [];
-storeTrack(initialPlaylist3, "1999", 4, "d");
-storeTrack(initialPlaylist3, "airlock", 5, "m");
-storeTrack(initialPlaylist3, "detroit 12am", 1, "m");
-//should not be compatible
-storeTrack(initialPlaylist3, "inconceivable", 8, "d");
-storeTrack(initialPlaylist3, "whyyyy", 1, "d");
-storeTrack(initialPlaylist3, "2005", 3, "m");
-console.log("Initial Playlist 3");
-//console.log(initialPlaylist3);
-console.log("Compatible with first song");
-console.log(listCompatible(initialPlaylist3));
-
-var initialPlaylist4 = initialPlaylist.concat(initialPlaylist2, initialPlaylist3);
-console.log("Initial Playlist 4");
-//console.log(initialPlaylist4);
-console.log("harmonic sort on Playlist 4");
-var sortedPlaylist4 = harmonicSort(initialPlaylist4);
-console.log(sortedPlaylist4);
-
-var initialPlaylist5 = [];
-storeTrack(initialPlaylist5, "timeless", 7, "m");
-storeTrack(initialPlaylist5, "for the moment", 6, "m");
-storeTrack(initialPlaylist5, "skylark", 3, "d");
-storeTrack(initialPlaylist5, "synthetic", 8, "m");
-storeTrack(initialPlaylist5, "numb the pain", 7, "m");
-storeTrack(initialPlaylist5, "on fire", 4, "m");
-storeTrack(initialPlaylist5, "one special", 3, "d");
-storeTrack(initialPlaylist5, "falling", 1, "m");
-storeTrack(initialPlaylist5, "pieces orig", 6, "m");
-storeTrack(initialPlaylist5, "pieces remix", 5, "d");
-storeTrack(initialPlaylist5, "dfrnt", 10, "d");
-storeTrack(initialPlaylist5, "for an angel", 12, "m");
-storeTrack(initialPlaylist5, "buckle up", 3, "m");
-storeTrack(initialPlaylist5, "confusia", 7, "m");
-storeTrack(initialPlaylist5, "walking clouds", 3, "m");
-storeTrack(initialPlaylist5, "locked up", 6, "m");
-storeTrack(initialPlaylist5, "emotion", 1, "m");
-storeTrack(initialPlaylist5, "muse", 10, "m");
-storeTrack(initialPlaylist5, "i can breathe", 9, "d");
-storeTrack(initialPlaylist5, "crush", 12, "m");
-storeTrack(initialPlaylist5, "sirens remix", 12, "m");
-storeTrack(initialPlaylist5, "600", 2, "m");
-storeTrack(initialPlaylist5, "spiral", 4, "d");
-storeTrack(initialPlaylist5, "air for life", 6, "m");
-console.log("Initial Playlist 5");
-//console.log(initialPlaylist5);
-console.log("harmonic sort of Playlist 5");
-console.log(harmonicSort(initialPlaylist5));
-
-var initialPlaylist6 = [];
-storeTrack(initialPlaylist6, "7m-1", 7, "m");
-storeTrack(initialPlaylist6, "5m-1", 5, "m");
-storeTrack(initialPlaylist6, "6m-1", 6, "m");
-storeTrack(initialPlaylist6, "4m-1", 4, "m");
-storeTrack(initialPlaylist6, "4m-2", 4, "m");
-storeTrack(initialPlaylist6, "6m-2", 6, "m");
-storeTrack(initialPlaylist6, "10m-1", 10, "m");
-storeTrack(initialPlaylist6, "10m-2", 10, "m");
-storeTrack(initialPlaylist6, "9m-1", 9, "m");
-console.log("Initial Playlist 6");
-//console.log(initialPlaylist6);
-console.log("harmonic sort of Playlist 6");
-console.log(harmonicSort(initialPlaylist6));
+// var initialPlaylist = [];
+// storeTrack(initialPlaylist, "Riders Of Rohan", 7, "m");
+// storeTrack(initialPlaylist, "Stargazer", 9, "m");
+// storeTrack(initialPlaylist, "Gamemaster", 2, "m");
+// storeTrack(initialPlaylist, "FAKE", 7, "d");
+// storeTrack(initialPlaylist, "FAKE2", 6, "d" );
+// storeTrack(initialPlaylist, "FAKE3", 10, "d");
+// storeTrack(initialPlaylist, "FAKE4", 6, "m");
+// storeTrack(initialPlaylist, "Khepera", 7, "m");
+// storeTrack(initialPlaylist, "Slave (feat. Fisher)", 8, "m");
+// //console.log("Initial Playlist");
+// //console.log(initialPlaylist);
+// console.log("Compatible with first song");
+// console.log(listCompatible(initialPlaylist, 7, "m"));
+//
+// var initialPlaylist2 = [];
+// storeTrack(initialPlaylist2, "here we go", 12, "m");
+// storeTrack(initialPlaylist2, "jump on it", 1, "m");
+// storeTrack(initialPlaylist2, "music and wine", 7, "m");
+// storeTrack(initialPlaylist2, "turn up the bass", 11, "d");
+// console.log("Initial Playlist 2");
+// //console.log(initialPlaylist2);
+// console.log("Compatible with first song");
+// console.log(listCompatible(initialPlaylist2, 12, "m"));
+//
+// var initialPlaylist3 = [];
+// storeTrack(initialPlaylist3, "1999", 4, "d");
+// storeTrack(initialPlaylist3, "airlock", 5, "m");
+// storeTrack(initialPlaylist3, "detroit 12am", 1, "m");
+// //should not be compatible
+// storeTrack(initialPlaylist3, "inconceivable", 8, "d");
+// storeTrack(initialPlaylist3, "whyyyy", 1, "d");
+// storeTrack(initialPlaylist3, "2005", 3, "m");
+// console.log("Initial Playlist 3");
+// //console.log(initialPlaylist3);
+// console.log("Compatible with first song");
+// console.log(listCompatible(initialPlaylist3, 4, "d"));
+//
+// var initialPlaylist4 = initialPlaylist.concat(initialPlaylist2, initialPlaylist3);
+// console.log("Initial Playlist 4");
+// //console.log(initialPlaylist4);
+// console.log("harmonic sort on Playlist 4");
+// var sortedPlaylist4 = harmonicSort(initialPlaylist4);
+// console.log(sortedPlaylist4);
+//
+// var initialPlaylist5 = [];
+// storeTrack(initialPlaylist5, "timeless", 7, "m");
+// storeTrack(initialPlaylist5, "for the moment", 6, "m");
+// storeTrack(initialPlaylist5, "skylark", 3, "d");
+// storeTrack(initialPlaylist5, "synthetic", 8, "m");
+// storeTrack(initialPlaylist5, "numb the pain", 7, "m");
+// storeTrack(initialPlaylist5, "on fire", 4, "m");
+// storeTrack(initialPlaylist5, "one special", 3, "d");
+// storeTrack(initialPlaylist5, "falling", 1, "m");
+// storeTrack(initialPlaylist5, "pieces orig", 6, "m");
+// storeTrack(initialPlaylist5, "pieces remix", 5, "d");
+// storeTrack(initialPlaylist5, "dfrnt", 10, "d");
+// storeTrack(initialPlaylist5, "for an angel", 12, "m");
+// storeTrack(initialPlaylist5, "buckle up", 3, "m");
+// storeTrack(initialPlaylist5, "confusia", 7, "m");
+// storeTrack(initialPlaylist5, "walking clouds", 3, "m");
+// storeTrack(initialPlaylist5, "locked up", 6, "m");
+// storeTrack(initialPlaylist5, "emotion", 1, "m");
+// storeTrack(initialPlaylist5, "muse", 10, "m");
+// storeTrack(initialPlaylist5, "i can breathe", 9, "d");
+// storeTrack(initialPlaylist5, "crush", 12, "m");
+// storeTrack(initialPlaylist5, "sirens remix", 12, "m");
+// storeTrack(initialPlaylist5, "600", 2, "m");
+// storeTrack(initialPlaylist5, "spiral", 4, "d");
+// storeTrack(initialPlaylist5, "air for life", 6, "m");
+// console.log("Initial Playlist 5");
+// //console.log(initialPlaylist5);
+// console.log("harmonic sort of Playlist 5");
+// console.log(harmonicSort(initialPlaylist5));
+//
+// var initialPlaylist6 = [];
+// storeTrack(initialPlaylist6, "7m-1", 7, "m");
+// storeTrack(initialPlaylist6, "5m-1", 5, "m");
+// storeTrack(initialPlaylist6, "6m-1", 6, "m");
+// storeTrack(initialPlaylist6, "4m-1", 4, "m");
+// storeTrack(initialPlaylist6, "4m-2", 4, "m");
+// storeTrack(initialPlaylist6, "6m-2", 6, "m");
+// storeTrack(initialPlaylist6, "10m-1", 10, "m");
+// storeTrack(initialPlaylist6, "10m-2", 10, "m");
+// storeTrack(initialPlaylist6, "9m-1", 9, "m");
+// console.log("Initial Playlist 6");
+// //console.log(initialPlaylist6);
+// console.log("harmonic sort of Playlist 6");
+// console.log(harmonicSort(initialPlaylist6));
